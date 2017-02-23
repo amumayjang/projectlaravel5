c5e3e7a236d2bbc84925cb95d643989dacadd9ad
amumayjang <lehuutruong999@gmail.com>
2017-02-23 17:27:50 +0700

    update new 23/02/2017

---
+++ amobile_site_home.php
@@ -0,0 +1,218 @@
+<html>
+    <head>
+        <title>A! Mobile Store</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="./public/plugins/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="./public/plugins/font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="./public/css/amobile_edit.css">
+        <!-- <link rel="stylesheet" type="text/css" href="./public/css/app.css"> -->
+    </head>
+    <body>
+        <section class="header-section">
+            <div class="top-bar">
+                <div class="container top-bar-container">
+                    <span class="text-wellcome">Chào mừng bạn đến với <a href="javascript:;">Amobile</a></span>
+                    <ul class="menu-login">
+                        <li>
+                            <a href="javascript:;"><i class="fa fa-unlock"></i> Đăng nhập <i class="fa fa-angle-down"></i></a>
+                        </li>
+                        <li>
+                            <a href="javascript:;"><i class="fa fa-user"></i> Tài khoản <i class="fa fa-angle-down"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="header">
+                <div class="container header-container">
+                    <div class="col-md-3 logo-header">
+                        <a href="javascript:;"><img src="./public/img/logo_amobile.png"></a>
+                    </div>
+                    <div class="col-md-6 search-header">
+                        <input class="col-md-9" type="text" placeholder="Nhập từ khóa tìm kiếm">
+                        <button class="col-md-3">Tìm kiếm</button>
+                        <ul class="menu-header">
+                            <li>
+                                <a href="javascript:;">Điện thoại</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">Máy tính bảng</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">Laptop</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">Máy cũ</a>
+                            </li>
+                            <li>
+                                <a href="javascript:;">Phụ kiện</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 support-address">
+                        <img src="./public/img/header-icon-support.png" >
+                        <span>
+                            <p>Hotline: <a href="javascript:;">1900 8198</a></p>
+                            <p>Email: <a href="javascript:;">support@amobile.com</a></p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="nav">
+                <div class="container nav-container">
+                    <ul class="menu-nav">
+                        <li class="menu-item">
+                            <a href="javascript:;">Trang chủ</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">Giới thiệu</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">Danh mục sản phẩm</a>
+                            <i class="fa fa-angle-down"></i>
+                            <ul class="menu-sub">
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">Khuyến mãi</a>
+                                </li>
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">Điện thoại</a>
+                                    <i class="fa fa-angle-right"></i>
+                                    <ul class="menu--sub">
+                                        <li class="menu--sub-item">
+                                            <a href="javascript:;">iPhone</a>
+                                        </li>
+                                        <li class="menu--sub-item">
+                                            <a href="javascript:;">Samsung</a>
+                                        </li>
+                                        <li class="menu--sub-item">
+                                            <a href="javascript:;">Oppo</a>
+                                        </li>
+                                        <li class="menu--sub-item">
+                                            <a href="javascript:;">Huawei</a>
+                                        </li>
+                                        <li class="menu--sub-item">
+                                            <a href="javascript:;">Asus</a>
+                                        </li>
+                                        <li class="menu--sub-item">
+                                            <a href="javascript:;">Acer</a>
+                                        </li>
+                                        <li class="menu--sub-item">
+                                            <a href="javascript:;">Khác</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">Máy tính bảng</a>
+                                </li>
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">Laptop</a>
+                                </li>
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">Máy cũ</a>
+                                </li>
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">Phụ kiện</a>
+                                </li>
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">Sạc dự phòng</a>
+                                </li>
+                                <li class="menu-sub-item">
+                                    <a href="javascript:;">SmarthWatch</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">Tin tức</a>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">Tuyển dụng</a>
+                        </li>
+                    </ul>
+                    <div class="cart-nav">
+                        <i class="fa fa-shopping-basket fa-3x"></i>
+                        <ul class="count-product">
+                            <li>
+                                <span>GIỎ HÀNG</span>
+                            </li>
+                            <li>
+                                <span id="count">0</span>
+                                <span>sản phẩm</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="content-section">
+            <div class="container first-module">
+                <div class="col-md-3 catalog">
+                    <ul class="title-catalog">
+                        <li>
+                            <i class="fa fa-bars fa-2x"></i>
+                        </li>
+                        <li>
+                            <h4>DANH MỤC SẢN PHẨM</h4>
+                        </li>
+                    </ul>
+                    <ul class="catalog-bg">
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-flash fa-2x"></i>&nbsp;&nbsp;Khuyến mãi</a>
+                        </li>
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-mobile-phone fa-2x"></i>&nbsp;&nbsp;Điện thoại&nbsp;<i class="fa fa-angle-right"></i></a>
+                            <ul class="menu--sub">
+                                <li class="menu--sub-item">
+                                    <a href="javascript:;">iPhone</a>
+                                </li>
+                                <li class="menu--sub-item">
+                                    <a href="javascript:;">Samsung</a>
+                                </li>
+                                <li class="menu--sub-item">
+                                    <a href="javascript:;">Oppo</a>
+                                </li>
+                                <li class="menu--sub-item">
+                                    <a href="javascript:;">Huawei</a>
+                                </li>
+                                <li class="menu--sub-item">
+                                    <a href="javascript:;">Asus</a>
+                                </li>
+                                <li class="menu--sub-item">
+                                    <a href="javascript:;">Acer</a>
+                                </li>
+                                <li class="menu--sub-item">
+                                    <a href="javascript:;">Khác</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-tablet fa-2x"></i>&nbsp;&nbsp;Máy tính bảng</a>
+                        </li>
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-laptop fa-2x"></i>&nbsp;&nbsp;Laptop</a>
+                        </li>
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-refresh fa-2x"></i>&nbsp;&nbsp;Máy cũ</a>
+                        </li>
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-headphones fa-2x"></i>&nbsp;&nbsp;Phụ kiện</a>
+                        </li>
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-battery fa-2x"></i>&nbsp;&nbsp;Sạc dự phòng</a>
+                        </li>
+                        <li class="menu-sub-item">
+                            <a href="javascript:;"><i class="fa fa-square-o fa-2x"></i>&nbsp;&nbsp;SmarthWatch</a>
+                        </li>
+                    </ul>
+                    <div class="product-catalog">
+                        
+                    </div>
+                </div>
+                <div class="col-md-9 product-show">
+                    
+                </div>
+            </div>
+        </section>
+        <section class="footer-section">
+
+        </section>
+    </body>
+</html>
